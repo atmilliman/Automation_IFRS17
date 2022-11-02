3e8271304965fba939e4b8e381075264c09bfccc
atmilliman <110993955+atmilliman@users.noreply.github.com>
2022-11-02 09:02:48 +0100

    Adding functionality
    The ability for the code automatically check current state of checks

--- checks.py
+++ checks.py
@@ -4,3 +4,19 @@ from datetime import date, datetime
 from email.message import EmailMessage
 from urllib.request import urlopen
 
+while True:
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+
+    if (current_time == "10:25:00") and (date.today().weekday() in [0, 1, 2, 3, 4]):
+        try:
+            str_date = str(date.today())
+            str_regex = r"(?<={}/).*?/".format(str_date)
+
+            day = str_date[-2:]
+            month = str_date[-5:-3]
+            year = str_date[:4]
+            str_date_eu = "{}.{}.{}".format(day, month, year)
+
+    else:
+        pass
